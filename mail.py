f35b45f414f6575136de18079d0a6814ac51539a
Avadh <81379791+Avadhkumar-geek@users.noreply.github.com>
2022-04-01 18:47:19 +0530

    sending mail using python

--- mail.py
+++ mail.py
@@ -2,7 +2,7 @@ import smtplib
 
 s = smtp.SMTP('smtp.gmail.com', 587)
 s.starttls()
-s.login('akglimitd@gmail.com', 'password')
+s.login('example@gmail.com', 'password')
 message = 'Subject: {}\n\n{}'.format('Hello', 'Hello Universe')
 s.sendmail('akglimited@gmail.com', 'yoyoavadh@gmail.com', message)
 s.quit()
